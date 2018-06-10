f579cacce14767b686c29c4ded282658f30a4f2d
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-10 17:07:47 +0800

    Add in code for endpoints and routes for users

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -18,7 +18,7 @@ defmodule RumblWeb.Router do
     pipe_through(:browser)
 
     get("/users", UserController, :index)
-    get("/", PageController, :index)
+    get("/users/:id", UserController, :show)
     get("/", PageController, :index)
   end
 
