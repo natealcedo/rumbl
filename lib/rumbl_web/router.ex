71822e4e091766f1afe82bb9434c9c3d4a721669
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-10 16:21:37 +0800

    Run mix format on codebase

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -2,21 +2,24 @@ defmodule RumblWeb.Router do
   use RumblWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", RumblWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/users", UserController, :index)
+    get("/", PageController, :index)
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
