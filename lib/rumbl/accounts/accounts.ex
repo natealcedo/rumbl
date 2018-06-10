6fd39258f2ac1ee23f322d5bb1b067a4bae953b3
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-10 16:19:49 +0800

    Add Accounts Context

---
+++ lib/rumbl/accounts/accounts.ex
@@ -0,0 +1,27 @@
+defmodule Rumble.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "José", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapids"},
+      %User{id: "3", name: "Chris", username: "chrismccord"}
+    ]
+  end
+
+  def get_user(id) do
+    list_users()
+    |> Enum.find(fn user -> user.id == id end)
+  end
+
+  def get_user_by(params) do
+    list_users()
+    |> Enum.find(fn user ->
+      Enum.all?(params, fn {key, val} -> Map.get(user, key) == val end)
+    end)
+  end
+end
