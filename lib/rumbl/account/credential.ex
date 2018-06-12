56147bfd78d719faae97310cdd363eb7805678f9
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-12 21:54:23 +0800

    Add ability to hash password and associate credential with user

--- lib/rumbl/account/credential.ex
+++ lib/rumbl/account/credential.ex
@@ -1,12 +1,14 @@
 defmodule Rumbl.Account.Credential do
   use Ecto.Schema
   import Ecto.Changeset
-
+  import Comeonin.Bcrypt
+  alias Rumbl.Account.User
 
   schema "credentials" do
-    field :email, :string
-    field :password_hash, :string
-    field :user_id, :id
+    field(:email, :string)
+    field(:password, :string, virtual: true)
+    field(:password_hash, :string)
+    belongs_to(:user, User)
 
     timestamps()
   end
@@ -14,8 +16,20 @@ defmodule Rumbl.Account.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
     |> unique_constraint(:email)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
