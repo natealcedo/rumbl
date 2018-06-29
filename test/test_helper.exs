b38f4da5d63808a49aa84ec768cc491e207806e3
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-30 00:17:33 +0800

    Add in test fixtures

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,36 @@
 ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
+
+defmodule Rumbl.TestHelpers do
+  alias Rumbl.{
+    Account,
+    Multimedia
+  }
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "Some User",
+        username: "user#{System.unique_integer([:positive])}",
+        credential: %{
+          password: "supersecret"
+        }
+      })
+      |> Account.register_user()
+
+    user
+  end
+
+  def video_fixture(%Account.User{} = user, attrs \\ %{}) do
+    attrs =
+      Enum.into(attrs, %{
+        url: "http://example.com",
+        description: "a description"
+      })
+
+    {:ok, video} = Multimedia.create_video(user, attrs)
+    video
+  end
+end
